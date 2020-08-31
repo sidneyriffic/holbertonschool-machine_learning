eaaa4a992eb8face9a9dcb89d5a110c9d9d87b5a
Sidney Burkett <sidneyriffic@gmail.com>
2020-08-30 20:49:11 -0700

    Fix linspace test point selection issue in 0x03-hyperparameter_tuning 3-bayes_opt.py

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -11,7 +11,7 @@ class BayesianOptimization:
     def __init__(self, f, X_init, Y_init, bounds, ac_samples,
                  l=1, sigma_f=1, xsi=0.01, minimize=True):
         self.f = f
-        self.X_s = np.linspace(bounds[0], bounds[1])[:, None]
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples)[:, None]
         self.xsi = xsi
         self.minimize = minimize
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
